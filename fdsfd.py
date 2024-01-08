242cc29825eff44d44dee4d6a39675d7ae858de3
EcimkhanEdil <149226320+EcimkhanEdil@users.noreply.github.com>
2024-01-08 12:38:41 +0600

    Add files via upload

--- fdsfd.py
+++ fdsfd.py
@@ -10,13 +10,16 @@ pygame.display.set_caption('TrueProger and FalseProger')
 #    webbrowser.open('https://www.youtube.com/watch?v=AqM-N4h3ViI')
 
 class Chvk():
-    def __init__(self,png,x,y,width,heigth):
+    def __init__(self,png,x,y,width,heigth,hp):
         self.rect=pygame.Rect(x, y, width, heigth)
         self.png=pygame.image.load(png)
+        self.hp=hp
     def drawd(self):
         linox.blit(self.png,(self.rect.x,self.rect.y))
     def fill(self):
         pygame.draw.rect(linox,self.fire)
+    def collidepoint(self,x,y):
+        return self.rect.collidepoint(x,y)
 
 class Bullet():
     def __init__(self,bul,damage,x_b, y_b, width_b, heigth_b):
@@ -29,14 +32,29 @@ class Bullet():
         linox.blit(self.bal,(self.coor_x,self.coor_y))
     def draw_fire_2(self):
         linox.blit(self.bal,(self.fire_2.x,self.fire_2.y))
+    def collidepoint(self,x,y):
+        return self.rect.collidepoint(x,y)
+
+class Bang():
+    def __init__(self,bang,x,y):
+        self.bang=pygame.image.load(bang)
+        self.x=x
+        self.y=y
+    def bangg(self):
+        linox.blit(self.bang,(self.x,self.y))
         
 
 f_x=250
 f_y=250
 
+kill=0
+
+x=0
+y=0
+
 bg=pygame.image.load('5.png')
-pl=Chvk('pl.png',250,250,50,100)
-en=Chvk('en.png',50,250,50,100)
+pl=Chvk('pl.png',250,250,50,100,100)
+en=Chvk('en.png',100,250,50,100,50)
 
 bull=10
 
@@ -44,11 +62,15 @@ bull=10
 
 temp=0
 
+bullett=Bullet('bullet.png',50,600,600,25,10)#
+bang=Bang('bang!.png',600,600)
+bung=0
+
 move_left=False
 move_right=False
 
 linox=pygame.display.set_mode((500,400))
-clock=pygame.time.Clock()#создание и сохранение времени в переменную
+clock=pygame.time.Clock()
 run=True
 #linox.fill((41, 227, 184))
 #font=pygame.font.Font(None,30).render('TrueProger_and_FalseProger',True,(227, 68, 248 ))
@@ -60,10 +82,12 @@ while run:
             run=False
             pygame.quit()
         if event.type==pygame.MOUSEBUTTONDOWN and event.button ==1:
-            bullett=Bullet('bullet.png',50,pl.rect.x,pl.rect.y,25,10)
+            bullett=Bullet('bullet.png',25,pl.rect.x-48,pl.rect.y+3,25,10)
             bullett.draw_fire()
             bullett.draw_fire_2()
-            bull=990
+            bang=Bang('bang!.png',pl.rect.x-60,pl.rect.y-5)
+            bull=160
+            bung=80
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_a:
                 move_left=True
@@ -84,9 +108,7 @@ while run:
     if move_right==True:
         pl.rect.x+=1
         
-    if bull<=1000 and bull>0:
-        bullett.draw_fire_2()
-        bull-=1
+    
     
     linox.blit(bg,(0,0))
     pl.drawd()
@@ -95,4 +117,26 @@ while run:
     if pl.rect.y<=220 and temp<=0:
         pl.rect.y=250
     if temp<=160:
-        temp-=1
+        temp-=15
+
+    if bull<=1000 and bull>0:
+        bullett.fire_2.x-=2
+        bullett.draw_fire_2()
+        bull-=1
+    if bung<=1000 and bung>0:
+        bang.bangg()
+        bung-=1
+        
+    if bullett.fire_2.x<=en.rect.x+40 and bullett.fire_2.y>=en.rect.y:
+        en.hp-=bullett.damage
+        bullett.fire_2.x=-100
+    if en.hp<=0 and kill:
+        en.x=-100
+        en.y=0
+        kill=100
+    if kill<=1000 and kill>0:
+        en.rect.y+=2
+        kill-=1
+    if en.rect.y>=250:
+        kill=0
+        en.rect.y=250
